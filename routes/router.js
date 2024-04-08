29f5b57e8b2af981a1049af5a9785fb5b801232d
Andy Lau <andyhplau@gmail.com>
2024-04-07 23:05:31 -0700

    implemented delete user pic

--- routes/router.js
+++ routes/router.js
@@ -320,6 +320,48 @@ router.get("/deletePetImage", async (req, res) => {
   }
 });
 
+router.get("/deleteUserImage", async (req, res) => {
+  try {
+    console.log("delete user image");
+
+    let user_id = req.query.id;
+
+    const schema = Joi.object({
+      user_id: Joi.string().alphanum().min(24).max(24).required(),
+    });
+
+    const validationResult = schema.validate({ user_id });
+
+    if (validationResult.error != null) {
+      console.log(validationResult.error);
+
+      res.render("error", { message: "Invalid user_id" });
+      return;
+    }
+
+    if (user_id) {
+      console.log("userId: " + user_id);
+      const success = await userCollection.updateOne(
+        { _id: new ObjectId(user_id) },
+        { $set: { image_id: undefined } },
+        {}
+      );
+
+      console.log("delete User Image: ");
+      console.log(success);
+      if (!success) {
+        res.render("error", { message: "Error connecting to MySQL" });
+        return;
+      }
+    }
+    res.redirect(`/`);
+  } catch (ex) {
+    res.render("error", { message: "Error connecting to MySQL" });
+    console.log("Error connecting to MySQL");
+    console.log(ex);
+  }
+});
+
 router.post("/addUser", async (req, res) => {
   try {
     console.log("form submit");
